a10fc8ed390035572144f314d7244b7bc9325fa4
UniFanor <UniFanor@FNS-WS-021688.EDUC.ACAD>
2025-03-20 20:46:55 -0300

    variavel

---
+++ variaveis.py
@@ -0,0 +1,11 @@
+# nome="Adler"
+# Criação e declaração da variavel
+# print(nome)
+# Mostrando mensagem na tela.
+
+# Entrada de dados no computador
+
+nomeCompleto = str(input(" Qual é o teu nome? "))
+print(nomeCompleto)
+
+# nomePessoa = input(str("Qual é o teu nome? "))
